8a3542a6d7035d405f77fbee8d7d8aeebed4642f
deeox2 <johann.doell@gmail.com>
2020-01-22 12:29:59 -0600

    Moving js to seperate file.

---
+++ pages/gradingscheme/scripts.js
@@ -0,0 +1,23 @@
+<script>
+  
+            var array = [["Assignments (4 assignments)","25%"],
+                         ["Class Project","30%"],
+                         ["Final Exam (3 hours)","45%"],
+                         ["Total","100%"]],
+                table = document.getElementById("table");
+           
+           for(var i = 0; i < array.length; i++)
+           {
+               // create a new row
+               var newRow = table.insertRow(table.length);
+               for(var j = 0; j < array[i].length; j++)
+               {
+                   // create a new cell
+                   var cell = newRow.insertCell(j);
+                   
+                   // add value to the cell
+                   cell.innerHTML = array[i][j];
+               }
+           }
+
+        </script>
